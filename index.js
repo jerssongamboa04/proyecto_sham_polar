aae2cd26238811d7dba89863d817df2367998350
jerssongamboa04 <118271417+jerssongamboa04@users.noreply.github.com>
2026-07-12 11:12:57 +0100

    Fix routes filename casing in API import

--- index.js
+++ index.js
@@ -2,8 +2,7 @@ require("dotenv").config();
 
 const express = require("express");
 const cors = require("cors");
-const router = require("./routes/ROUTES.JS");
-
+const router = require("./routes/routes");
 const app = express();
 
 const PORT = process.env.PORT || 8000;
